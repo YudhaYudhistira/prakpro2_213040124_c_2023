6a9008b854c2ee5a535ee96ba712dd2646075a2b
YudhaYudhistira <yudhistirayudha09@gmail.com>
2023-10-25 11:43:29 +0700

    perubahan

--- FormApp.java
+++ FormApp.java
@@ -20,6 +20,19 @@ public class FormApp extends JFrame {
 
     public FormApp() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+            // Menambahkan WindowListener
+        this.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                int confirm = JOptionPane.showOptionDialog(
+                    null, "Anda yakin ingin keluar?", "Konfirmasi Keluar", JOptionPane.YES_NO_OPTION, 
+                    JOptionPane.QUESTION_MESSAGE, null, null, null);
+
+                if (confirm == JOptionPane.YES_OPTION) {
+                    System.exit(0); // Menutup aplikasi jika pengguna memilih "Ya"
+                }
+            }
+        });
 
         // Label dan kotak teks untuk input Nama
         JLabel labelNama = new JLabel("Nama:");
@@ -32,60 +45,71 @@ public class FormApp extends JFrame {
         labelNomorTelepon.setBounds(15, 100, 150, 10);
         JTextField textFieldNomorTelepon = new JTextField();
         textFieldNomorTelepon.setBounds(15, 120, 350, 30);
+        //Label kelamin
+        JLabel labelKelamin = new JLabel("Jenis Kelamin:");
+        labelKelamin.setBounds(15, 160, 150, 10);
 
         // RadioButton untuk memilih jenis kelamin
-        JRadioButton radioButtonLakiLaki = new JRadioButton("Laki-Laki");
-        radioButtonLakiLaki.setBounds(15, 160, 100, 30);
-        JRadioButton radioButtonPerempuan = new JRadioButton("Perempuan");
-        radioButtonPerempuan.setBounds(125, 160, 100, 30);
-        ButtonGroup radioGroupKelamin = new ButtonGroup();
-        radioGroupKelamin.add(radioButtonLakiLaki);
-        radioGroupKelamin.add(radioButtonPerempuan);
+          JRadioButton radioButton_1 = new JRadioButton("Laki-laki", true);
+        radioButton_1.setBounds(15, 170, 200, 30);
+         JRadioButton radioButton_2 = new JRadioButton("Perempuan");
+        radioButton_2.setBounds(15, 190, 200, 30);
+        
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(radioButton_1);
+        bg.add(radioButton_2);
 
         // Label dan TextArea untuk Alamat
         JLabel labelAlamat = new JLabel("Alamat:");
-        labelAlamat.setBounds(15, 200, 150, 10);
+        labelAlamat.setBounds(15, 220, 150, 10);
         JTextArea textAreaAlamat = new JTextArea();
-        textAreaAlamat.setBounds(15, 220, 350, 80);
+        textAreaAlamat.setBounds(15, 240, 350, 80);
 
         // Tombol untuk menyimpan data
         JButton buttonSimpan = new JButton("Simpan");
-        buttonSimpan.setBounds(15, 320, 100, 40);
+        buttonSimpan.setBounds(15, 330, 100, 40);
 
         // Tabel untuk menampilkan data
         String[] columnNames = {"Nama", "Jenis Kelamin", "Nomor Telepon", "Alamat"};
         tableModel = new DefaultTableModel(columnNames, 0);
         table = new JTable(tableModel);
         JScrollPane scrollPane = new JScrollPane(table);
-        scrollPane.setBounds(15, 370, 450, 150);
+        scrollPane.setBounds(15, 380, 450, 150);
 
-        // Mendengarkan klik tombol "Simpan"
+        // Mendengarkan klik tombol "Simpan"           
         buttonSimpan.addActionListener(e -> {
-            String nama = textFieldNama.getText();
-            String nomorTelepon = textFieldNomorTelepon.getText();
-            String jenisKelamin = radioButtonLakiLaki.isSelected() ? "Laki-Laki" : "Perempuan";
-            String alamat = textAreaAlamat.getText();
-
+            int confirm = JOptionPane.showOptionDialog(
+                this, "Anda yakin ingin menyimpan data?", "Konfirmasi Simpan", JOptionPane.YES_NO_OPTION, 
+                JOptionPane.QUESTION_MESSAGE, null, null, null);
+            if (confirm == JOptionPane.YES_OPTION) {
+                String nama = textFieldNama.getText();
+                String nomorTelepon = textFieldNomorTelepon.getText();
+                String jenisKelamin = radioButton_1.isSelected() ? "Laki-Laki" : "Perempuan";
+                String alamat = textAreaAlamat.getText();
+            
+            
             if (nama.isEmpty() || nomorTelepon.isEmpty() || alamat.isEmpty()) {
                 JOptionPane.showMessageDialog(this, "Ada field yang belum diisi.", "Peringatan", JOptionPane.WARNING_MESSAGE);
             } else {
                 tableModel.addRow(new Object[] {nama, jenisKelamin, nomorTelepon, alamat});
                 textFieldNama.setText("");
                 textFieldNomorTelepon.setText("");
-                radioButtonLakiLaki.setSelected(false);
-                radioButtonPerempuan.setSelected(false);
+                radioButton_1.setSelected(false);
+                radioButton_2.setSelected(false);
                 textAreaAlamat.setText("");
             }
+           }
         });
 
         // Menambahkan komponen-komponen ke dalam frame
         this.add(buttonSimpan);
         this.add(textFieldNama);
         this.add(textFieldNomorTelepon);
-        this.add(radioButtonLakiLaki);
-        this.add(radioButtonPerempuan);
+        this.add(radioButton_1);
+        this.add(radioButton_2);
         this.add(labelNama);
         this.add(labelNomorTelepon);
+        this.add(labelKelamin);
         this.add(labelAlamat);
         this.add(textAreaAlamat);
         this.add(scrollPane);
